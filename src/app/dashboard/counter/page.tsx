4e2fb89fc99a1903f1b142ca691efb0383151833
Abel Borit Guitton <abelboritguitton@gmail.com>
2025-07-25 22:52:33 -0500

    [update] CounterPage

--- src/app/dashboard/counter/page.tsx
+++ src/app/dashboard/counter/page.tsx
@@ -1,3 +1,21 @@
-export default function CounterPage() {
-  return <div>CounterPage</div>;
+import type { Metadata } from 'next';
+import { CartCounter } from '@/app/shopping-cart/components';
+
+interface CounterPageProps {
+  value?: number;
+}
+
+export const metadata: Metadata = {
+  description: 'Este es el Counter Page',
+  title: 'Counter Page',
+};
+
+export default function CounterPage({ value = 10 }: CounterPageProps) {
+  return (
+    <div className="flex flex-col items-center justify-center w-full h-full">
+      <span>Productos en el carrito de compras</span>
+
+      <CartCounter value={value} />
+    </div>
+  );
 }
